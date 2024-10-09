2729278de0660e58a5d1981cc73d131a7ac107a3
thenishantgiri <nishantkrgiri@gmail.com>
2024-10-10 01:47:12 +0530

    Dashboard: tailwind resposive layout implemented

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,31 @@
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">AcademiSys</span>
+        </Link>
+
+        <Menu />
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
+        right
+      </div>
+    </div>
+  );
 }
