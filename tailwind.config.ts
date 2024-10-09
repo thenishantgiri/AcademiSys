80688b5c122a8a2d7794b565a91802aa67456870
thenishantgiri <nishantkrgiri@gmail.com>
2024-10-10 03:55:42 +0530

    AdminPage: dashboard design implemented

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        laSky: "#C3EBFA",
+        laSkyLight: "#EDF9FD",
+        laPurple: "#CFCEFF",
+        laPurpleLight: "#F1F0FF",
+        laYellow: "#FAE27C",
+        laYellowLight: "#FEFCE8",
+      },
     },
   },
   plugins: [],
