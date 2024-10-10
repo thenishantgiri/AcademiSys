780a1602f2bb7f2cb1dbf1505e9318ac8e587587
thenishantgiri <nishantkrgiri@gmail.com>
2024-10-10 11:36:46 +0530

    TeacherPage: BigCalendar - ui implemented

--- app/(dashboard)/teacher/page.tsx
+++ app/(dashboard)/teacher/page.tsx
@@ -1,7 +1,21 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+
 const TeacherPage = () => {
   return (
-    <div className="">
-      <h1>TeacherPage</h1>
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
     </div>
   );
 };
