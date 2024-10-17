485fa2dd9e0f35e43279199ee7eaa237e66740ab
thenishantgiri <nishantkrgiri@gmail.com>
2024-10-17 07:25:16 +0530

    StudentDashboardPage: ui implemented

--- app/(dashboard)/list/students/page.tsx
+++ app/(dashboard)/list/students/page.tsx
@@ -73,7 +73,7 @@ const StudentListPage = () => {
       <td className="hidden md:table-cell">{item.address}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
+          <Link href={`/list/students/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-laSky">
               <Image src="/view.png" alt="view" width={16} height={16} />
             </button>
