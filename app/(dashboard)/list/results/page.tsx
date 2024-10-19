fae84ec8437560959e210f788830cab360541539
thenishantgiri <nishantkrgiri@gmail.com>
2024-10-19 11:02:41 +0530

    EventListPage: fetched events

--- app/(dashboard)/list/results/page.tsx
+++ app/(dashboard)/list/results/page.tsx
@@ -58,7 +58,7 @@ const columns = [
 const renderRow = (item: ResultList) => (
   <tr
     key={item.id}
-    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-laPurpleLight"
   >
     <td className="flex items-center gap-4 p-4">{item.title}</td>
     <td>{item.studentName + " " + item.studentSurname}</td>
