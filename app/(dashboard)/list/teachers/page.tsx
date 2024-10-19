ec937ec69ba8b1807c8321f1c3dcd1541862a14a
thenishantgiri <nishantkrgiri@gmail.com>
2024-10-19 07:25:38 +0530

    SearchFilter: fetched data with Search Params on URL

--- app/(dashboard)/list/teachers/page.tsx
+++ app/(dashboard)/list/teachers/page.tsx
@@ -117,6 +117,9 @@ const TeacherListPage = async ({
               },
             };
             break;
+          case "search":
+            query.name = { contains: value, mode: "insensitive" };
+            break;
           default:
             break;
         }
